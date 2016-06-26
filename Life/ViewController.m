9e4a8dabe4f09233030ac73d080b9f264db1f046
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-25 21:10:47 -0600

    UI improvements

--- Life/ViewController.m
+++ Life/ViewController.m
@@ -21,14 +21,29 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    UIColor* skyBlue = [UIColor colorWithRed:135.0/255.0 green:206.0/255.0 blue:250.0/255.0 alpha:1.0];
+    UIColor* lightPurple = [UIColor colorWithRed:250.0/255.0 green:180.0/255.0 blue:135.0/255.0 alpha:1.0];
+    self.view.backgroundColor = skyBlue;
+    
     CGFloat width = self.view.frame.size.width;
-    _gridView = [[LFGridView alloc] initWithFrame:CGRectMake(0, self.view.center.y / 4, width, width) gridLength:20];
+    _gridView = [[LFGridView alloc] initWithFrame:CGRectMake(0, self.view.center.y / 1.75, width, width) gridLength:20];
     [self.view addSubview:_gridView];
+    
+    UILabel* titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, _gridView.frame.origin.y)];
+    titleLabel.center = CGPointMake(self.view.center.x, titleLabel.frame.size.height / 2);
+    titleLabel.text = @"Conway's Game of Life";
+    titleLabel.numberOfLines = 2;
+    titleLabel.adjustsFontSizeToFitWidth = YES;
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.font = [UIFont fontWithName:@"MarkerFelt-Thin" size:50];
+    [self.view addSubview:titleLabel];
     
     UIButton* start = [UIButton buttonWithType:UIButtonTypeSystem];
     [start setTitle:@"Start" forState:UIControlStateNormal];
-    start.frame = CGRectMake(0, 0, self.view.frame.size.width / 4, self.view.frame.size.width / 8);
-    start.center = CGPointMake(self.view.center.x, _gridView.frame.origin.y + _gridView.frame.size.height + start.frame.size.height);
+    [start setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    start.titleLabel.font = [UIFont fontWithName:@"MarkerFelt-Thin" size:start.titleLabel.font.pointSize * 1.5];
+    start.frame = CGRectMake(0, _gridView.frame.origin.y + _gridView.frame.size.height, self.view.frame.size.width, self.view.frame.size.width / 8);
+    start.backgroundColor = lightPurple;
     [start addTarget:self action:@selector(startButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:start];
 }

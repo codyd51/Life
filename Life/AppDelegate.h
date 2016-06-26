1f1f2fa3172a1244cb656d4d340c15e273b2db85
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-25 18:30:24 -0600

    Initial Commit

---
+++ Life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Life
+//
+//  Created by Phillip Tennen on 6/25/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Life/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Life
+//
+//  Created by Phillip Tennen on 6/25/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
